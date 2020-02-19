abd5c390243e8eb4c4d55e5c3c5c18d5f1141cc1
andredegaut <52792973+andredegaut@users.noreply.github.com>
2020-02-19 14:13:01 -0300

    Cors

--- src/main/java/com/degaut/course/resources/ProductResource.java
+++ src/main/java/com/degaut/course/resources/ProductResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.degaut.course.entities.Product;
 import com.degaut.course.services.ProductService;
 
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping(value= "/products")
 public class ProductResource {
